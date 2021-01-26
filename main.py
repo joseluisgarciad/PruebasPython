28b3b93651481cf6b6941d45c0168f5f8944d73b
0015648 <jlgarciad@viewnext.com>
2021-01-26 12:13:19 +0100

    Limpieza codigo

--- main.py
+++ main.py
@@ -3,36 +3,28 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def listas(lista:list):
-    print("lista1 : ")
+def addalistas(lista: list):
+    print("lista : ")
     print(lista)
-    Listavacia = []
-    for i in range(0,5,1):
-        Listavacia.append(int(input("Dame el valor nº:".format(i-1))))
-    print(Listavacia)
+    listavacia = []
+    for i in range(0, 5, 1):
+        listavacia.append(int(input("Dame el valor nº:".format(i - 1))))
+    print(listavacia)
+    for i in range(0, 5, 1):
+        print(listavacia[i])
 
-    for i in range(0,5,1):
-        print(Listavacia[i])
 
-def Añadir_a_tupla(tupla:tuple, num):
-    LisTemp = list(tupla)
+def addatupla(tupla: tuple, num):
+    listemp = list(tupla)
 
-    LisTemp.append(num)
+    listemp.append(num)
 
-    tupla=tuple(LisTemp)
+    tupla = tuple(listemp)
     return tupla
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    listas([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    tupla=(23, 34, 65, 23, 44)
-    print(Añadir_a_tupla(tupla, input("Dame el valor para añadir a Tupla:")))
+addalistas([1, 2, 3, 4, 5, 6, 7, 8, 9])
+tupla = (23, 34, 65, 23, 44)
+print(addatupla(tupla, input("Dame el valor para añadir a Tupla:")))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
